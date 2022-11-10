05db9c4dabd0fb419b8c5eef5333190fb96622c9
puppets101 <oscand92@gmail.com>
2022-11-10 23:52:06 +0100

    Deleted unused components/files

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Category } from '../interfaces';
+import { categories } from '../utils/category-data';
 import CategoryItem from './CategoryItem';
 
 const Category: FC = () => {
@@ -11,36 +12,5 @@ const Category: FC = () => {
     </div>
   );
 };
-
-const categories: Category[] = [
-  {
-    id: '1',
-    label: 'MAN',
-    imageUrl:
-      'https://images.unsplash.com/photo-1489913590284-9269438bb411?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    description: 'Kategori man',
-  },
-  {
-    id: '2',
-    label: 'TJEJ',
-    imageUrl:
-      'https://plus.unsplash.com/premium_photo-1663047595724-cd5baf9cbcf1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    description: 'Kategori tjej',
-  },
-  {
-    id: '3',
-    label: 'NYHETER',
-    imageUrl:
-      'https://images.unsplash.com/photo-1445108771252-d1cc31a02a3c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80',
-    description: 'Kategori nyheter',
-  },
-  {
-    id: '4',
-    label: 'OM VARG',
-    imageUrl:
-      'https://images.unsplash.com/photo-1470246973918-29a93221c455?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
-    description: 'Kategori om varg',
-  },
-];
 
 export default Category;
